ce8a209c1a020d34aaff96e930a39b4228e37635
Marvin Humphrey <marvin@apache.org>
2006-10-17 00:19:31 +0000

    Add placeholder POD to Lucy.pm.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/lucy/trunk@464755 13f79535-47bb-0310-9956-ffa450edef68

--- src/perl/Lucy.pm
+++ src/perl/Lucy.pm
@@ -6,3 +6,37 @@ our $VERSION = 0.00_01;
 
 1;
 
+=head1 NAME
+
+Lucy - Search engine library.
+
+=head1 SYNOPSIS
+
+To do.
+
+=head1 DESCRIPTION
+
+To do.
+
+=head1 AUTHOR
+
+Marvin Humphrey E<lt>marvin at rectangular dot comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+    /**
+     * Copyright 2006 The Apache Software Foundation
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+     * implied.  See the License for the specific language governing
+     * permissions and limitations under the License.
+     */
+
