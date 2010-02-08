b939877f6bd54d9452a4c0ca56838341bd0bc1e8
Marvin Humphrey <marvin@apache.org>
2010-02-08 19:20:01 +0000

    Add a probe for OSAtomicCompareAndSwapPtr, since that's not available on OS X Tiger and the probe for OSAtomic.h on its own is insufficient.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/lucy/trunk@907761 13f79535-47bb-0310-9956-ffa450edef68

--- charmonizer/src/Charmonizer/Probe/AtomicOps.c
+++ charmonizer/src/Charmonizer/Probe/AtomicOps.c
@@ -1,25 +1,46 @@
 #define CHAZ_USE_SHORT_NAMES
 
+#include "Charmonizer/Core/Compiler.h"
 #include "Charmonizer/Core/HeaderChecker.h"
 #include "Charmonizer/Core/ConfWriter.h"
 #include "Charmonizer/Core/Util.h"
 #include "Charmonizer/Probe/AtomicOps.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+static char osatomic_casptr_code[] = 
+    QUOTE(  #include <libkern/OSAtomic.h>                                  )
+    QUOTE(  #include <libkern/OSAtomic.h>                                  )
+    QUOTE(  int main() {                                                   )
+    QUOTE(      int  foo = 1;                                              )
+    QUOTE(      int *foo_ptr = &foo;                                       )
+    QUOTE(      int *target = NULL;                                        )
+    QUOTE(      OSAtomicCompareAndSwapPtr(NULL, foo_ptr, (void**)&target); )
+    QUOTE(      return 0;                                                  )
+    QUOTE(  }                                                              );
 
 void
 AtomicOps_run(void) 
 {
-    chaz_bool_t has_libkern_osatomic_h = false;
-    chaz_bool_t has_sys_atomic_h       = false;
-    chaz_bool_t has_intrin_h           = false;
+    chaz_bool_t  has_libkern_osatomic_h = false;
+    chaz_bool_t  has_osatomic_cas_ptr   = false;
+    chaz_bool_t  has_sys_atomic_h       = false;
+    chaz_bool_t  has_intrin_h           = false;
 
     ConfWriter_start_module("AtomicOps");
 
     if (HeadCheck_check_header("libkern/OSAtomic.h")) {
         has_libkern_osatomic_h = true;
         ConfWriter_append_conf("#define CHY_HAS_LIBKERN_OSATOMIC_H\n");
+
+        /* Check for OSAtomicCompareAndSwapPtr, introduced in later versions
+         * of OSAtomic.h. */
+        has_osatomic_cas_ptr = CC_test_compile(osatomic_casptr_code,
+            strlen(osatomic_casptr_code));
+        if (has_osatomic_cas_ptr) {
+            ConfWriter_append_conf("#define CHY_HAS_OSATOMIC_CAS_PTR\n");
+        }
     }
     if (HeadCheck_check_header("sys/atomic.h")) {
         has_sys_atomic_h = true;
@@ -36,6 +57,9 @@ AtomicOps_run(void)
     ConfWriter_start_short_names();
     if (has_libkern_osatomic_h) {
         ConfWriter_shorten_macro("HAS_LIBKERN_OSATOMIC_H");
+        if (has_osatomic_cas_ptr) {
+            ConfWriter_shorten_macro("HAS_OSATOMIC_CAS_PTR");
+        }
     }
     if (has_sys_atomic_h) {
         ConfWriter_shorten_macro("HAS_SYS_ATOMIC_H");
--- charmonizer/src/Charmonizer/Probe/AtomicOps.h
+++ charmonizer/src/Charmonizer/Probe/AtomicOps.h
@@ -18,6 +18,10 @@ extern "C" {
  * HAS_LIBKERN_OSATOMIC_H  <libkern/OSAtomic.h> (Mac OS X)
  * HAS_SYS_ATOMIC_H        <sys/atomic.h>       (Solaris)
  * HAS_INTRIN_H            <intrin.h>           (Windows)
+ *
+ * This symbol is defined if OSAtomicCompareAndSwapPtr is available:
+ *
+ * HAS_OSATOMIC_CAS_PTR
  */
 void chaz_AtomicOps_run(void);
 
