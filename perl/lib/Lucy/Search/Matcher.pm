c4a1a5d03a091a52e9b7428ffcd1fc8a2cfe958b
Marvin Humphrey <marvin@apache.org>
2010-06-18 01:16:28 +0000

    LUCY-111: Add Lucy::Search::Matcher.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/lucy/trunk@955810 13f79535-47bb-0310-9956-ffa450edef68

---
+++ perl/lib/Lucy/Search/Matcher.pm
@@ -0,0 +1,49 @@
+package Lucy::Search::Matcher;
+use Lucy;
+
+1;
+
+__END__
+
+__BINDING__
+
+my $synopsis = <<'END_SYNOPSIS';
+    # Matcher is an abstract base class -- see MockScorer for an example
+    # implementation.
+END_SYNOPSIS
+
+my $constructor = <<'END_CONSTRUCTOR_CODE_SAMPLE';
+    my $matcher = MyMatcher->SUPER::new;
+END_CONSTRUCTOR_CODE_SAMPLE
+
+Clownfish::Binding::Perl::Class->register(
+    parcel            => "Lucy",
+    class_name        => "Lucy::Search::Matcher",
+    bind_methods      => [qw( Next Advance Get_Doc_ID Score Collect )],
+    bind_constructors => ["new"],
+    make_pod          => {
+        synopsis    => $synopsis,
+        constructor => { sample => $constructor },
+        methods     => [qw( next advance get_doc_id score )],
+    }
+);
+
+__COPYRIGHT__
+
+
+    /**
+     * Copyright 2010 The Apache Software Foundation
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+     * implied.  See the License for the specific language governing
+     * permissions and limitations under the License.
+     */
+
