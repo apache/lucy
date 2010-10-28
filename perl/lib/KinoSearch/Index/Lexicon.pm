45f4c79798ee5b62ca33f225224bf01e647a6537
Marvin Humphrey <marvin@apache.org>
2010-10-28 21:50:16 +0000

    Fix outdated sample code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1028511 13f79535-47bb-0310-9956-ffa450edef68

--- perl/lib/KinoSearch/Index/Lexicon.pm
+++ perl/lib/KinoSearch/Index/Lexicon.pm
@@ -23,7 +23,8 @@ __END__
 __BINDING__
 
 my $synopsis = <<'END_SYNOPSIS';
-    my $lexicon = $index_reader->lexicon( field => 'content' );
+    my $lex_reader = $seg_reader->obtain('KinoSearch::Index::LexiconReader');
+    my $lexicon = $lex_reader->lexicon( field => 'content' );
     while ( $lexicon->next ) {
        print $lexicon->get_term . "\n";
     }
