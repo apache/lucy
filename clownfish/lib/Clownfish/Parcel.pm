fd8d7c61d15c37053a865dfa644e734ee77df913
Marvin Humphrey <marvin@apache.org>
2010-11-03 04:50:52 +0000

    Hard-code the cnick for the KinoSearch parcel to "Kino" with a temporary hack to Clownfish, allowing us to remove all the explicit cnick args in parcel declarations.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1030331 13f79535-47bb-0310-9956-ffa450edef68

--- clownfish/lib/Clownfish/Parcel.pm
+++ clownfish/lib/Clownfish/Parcel.pm
@@ -41,6 +41,12 @@ sub singleton {
     verify_args( \%singleton_PARAMS, %args ) or confess $@;
     my ( $name, $cnick ) = @args{qw( name cnick )};
 
+    # Temporary hack.
+    if ( $name eq 'KinoSearch' ) {
+        $cnick = 'Kino';
+        $args{cnick} = 'Kino';
+    }
+
     # Return the default parcel for either a blank name or an undefined name.
     return $default_parcel unless $name;
 
