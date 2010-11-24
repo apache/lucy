b904618ed416d1069141d7c25fa5e4b8319ddacc
Marvin Humphrey <marvin@apache.org>
2010-11-24 02:28:00 +0000

    LUCY-125 Add tests which verify the basic functionality of our wrappers for the Snowball stemming librariy.  (test_snowstem.patch, snowstem_test_content.patch)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1038445 13f79535-47bb-0310-9956-ffa450edef68

--- core/Lucy/Test/Analysis/TestStemmer.c
+++ core/Lucy/Test/Analysis/TestStemmer.c
@@ -20,6 +20,8 @@
 #include "Lucy/Test.h"
 #include "Lucy/Test/Analysis/TestStemmer.h"
 #include "Lucy/Analysis/Stemmer.h"
+#include "Lucy/Store/FSFolder.h"
+#include "Lucy/Util/Json.h"
 
 static void
 test_Dump_Load_and_Equals(TestBatch *batch)
@@ -48,14 +50,59 @@ test_Dump_Load_and_Equals(TestBatch *batch)
     DECREF(other_clone);
 }
 
+static void
+test_stemming(TestBatch *batch)
+{
+    CharBuf  *path           = CB_newf("modules");
+    FSFolder *modules_folder = FSFolder_new(path);
+    if (!FSFolder_Check(modules_folder)) {
+        DECREF(modules_folder);
+        CB_setf(path, "../modules"); 
+        modules_folder = FSFolder_new(path);
+        if (!FSFolder_Check(modules_folder)) {
+            THROW(ERR, "Can't open modules folder");
+        }
+    }
+    CB_setf(path, "analysis/snowstem/source/test/tests.json");
+    Hash *tests = (Hash*)Json_slurp_json((Folder*)modules_folder, path);
+    if (!tests) { RETHROW(Err_get_error()); }
+
+    CharBuf *iso;
+    Hash *lang_data;
+    Hash_Iterate(tests);
+    while (Hash_Next(tests, (Obj**)&iso, (Obj**)&lang_data)) {
+        VArray  *words   = (VArray*)Hash_Fetch_Str(lang_data, "words", 5);
+        VArray  *stems   = (VArray*)Hash_Fetch_Str(lang_data, "stems", 5);
+        Stemmer *stemmer = Stemmer_new(iso);
+        for (uint32_t i = 0, max = VA_Get_Size(words); i < max; i++) {
+            CharBuf *word  = (CharBuf*)VA_Fetch(words, i);
+            VArray  *got   = Stemmer_Split(stemmer, word);
+            CharBuf *stem  = (CharBuf*)VA_Fetch(got, 0);
+            TEST_TRUE(batch,
+                   stem
+                && CB_Is_A(stem, CHARBUF)
+                && CB_Equals(stem, VA_Fetch(stems, i)),
+                "Stem %s: %s", CB_Get_Ptr8(iso), CB_Get_Ptr8(word)
+                );
+            DECREF(got);
+        }
+        DECREF(stemmer);
+    }
+
+    DECREF(tests);
+    DECREF(modules_folder);
+    DECREF(path);
+}
+
 void
 TestStemmer_run_tests()
 {
-    TestBatch *batch = TestBatch_new(3);
+    TestBatch *batch = TestBatch_new(153);
 
     TestBatch_Plan(batch);
 
     test_Dump_Load_and_Equals(batch);
+    test_stemming(batch);
 
     DECREF(batch);
 }
