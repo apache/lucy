a46b510116f5418de8d2fb803e0b9631562e5c01
Marvin Humphrey <marvin@apache.org>
2011-07-18 21:26:58 +0000

    Updating CHANGES and version number for release 0.2.0.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1148057 13f79535-47bb-0310-9956-ffa450edef68

--- perl/lib/Lucy.pm
+++ perl/lib/Lucy.pm
@@ -21,12 +21,12 @@ package Lucy;
 use 5.008003;
 use Exporter;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.002000';
 $VERSION = eval $VERSION;
 
 use XSLoader;
 # This loads a large number of disparate subs.
-BEGIN { XSLoader::load( 'Lucy', '0.001000' ) }
+BEGIN { XSLoader::load( 'Lucy', '0.002000' ) }
 
 BEGIN {
     push our @ISA, 'Exporter';
