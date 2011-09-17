8447b8cea6b868e6dcdd86ead409092006dc172e
Marvin Humphrey <marvin@apache.org>
2011-09-17 21:36:32 +0000

    Merge StringHelper3.c into StringHelper.c, remove Unicode license.
    This file StringHelper3.c, which contains only a single function
    StrHelp_is_whitespace(), is currently listed under the Unicode Consortium's
    license.  I wrote that file myself, but erring on the side of paranoia, I
    credited the Unicode Consortium for having defined the character class.  I now
    believe that this was incorrect, and I am not aware of any other projects
    that have done likewise.  StrHelp_is_whitespace() implements a function using
    the Unicode standard; it is not a modified redistribution of Unicode data
    files.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1172071 13f79535-47bb-0310-9956-ffa450edef68

--- core/Lucy/Util/StringHelper.c
+++ core/Lucy/Util/StringHelper.c
@@ -129,6 +129,32 @@ StrHelp_utf8_valid(const char *ptr, size_t size) {
     return true;
 }
 
+bool_t
+StrHelp_is_whitespace(uint32_t code_point) {
+    switch (code_point) {
+            // <control-0009>..<control-000D>
+        case 0x0009: case 0x000A: case 0x000B: case 0x000C: case 0x000D:
+        case 0x0020: // SPACE
+        case 0x0085: // <control-0085>
+        case 0x00A0: // NO-BREAK SPACE
+        case 0x1680: // OGHAM SPACE MARK
+        case 0x180E: // MONGOLIAN VOWEL SEPARATOR
+            // EN QUAD..HAIR SPACE
+        case 0x2000: case 0x2001: case 0x2002: case 0x2003: case 0x2004:
+        case 0x2005: case 0x2006: case 0x2007: case 0x2008: case 0x2009:
+        case 0x200A:
+        case 0x2028: // LINE SEPARATOR
+        case 0x2029: // PARAGRAPH SEPARATOR
+        case 0x202F: // NARROW NO-BREAK SPACE
+        case 0x205F: // MEDIUM MATHEMATICAL SPACE
+        case 0x3000: // IDEOGRAPHIC SPACE
+            return true;
+
+        default:
+            return false;
+    }
+}
+
 uint32_t
 StrHelp_encode_utf8_char(uint32_t code_point, void *buffer) {
     uint8_t *buf = (uint8_t*)buffer;
