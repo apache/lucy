4f0632641177ce8d817dafea1b52623be8997527
Nikolas Wellnhofer <nwellnhof@apache.org>
2011-12-22 22:29:50 +0000

    Don't derive EasyAnalyzer from PolyAnalyzer
    Make EasyAnalyzer a direct subclass of Analyzer and implement all needed
    methods.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1222495 13f79535-47bb-0310-9956-ffa450edef68

--- core/Lucy/Analysis/EasyAnalyzer.c
+++ core/Lucy/Analysis/EasyAnalyzer.c
@@ -18,7 +18,7 @@
 #include "Lucy/Util/ToolSet.h"
 
 #include "Lucy/Analysis/EasyAnalyzer.h"
-#include "Lucy/Analysis/PolyAnalyzer.h"
+#include "Lucy/Analysis/Inversion.h"
 #include "Lucy/Analysis/StandardTokenizer.h"
 #include "Lucy/Analysis/Normalizer.h"
 #include "Lucy/Analysis/SnowballStemmer.h"
@@ -31,13 +31,70 @@ EasyAnalyzer_new(const CharBuf *language) {
 
 EasyAnalyzer*
 EasyAnalyzer_init(EasyAnalyzer *self, const CharBuf *language) {
-    VArray *analyzers = VA_new(3);
-    VA_Push(analyzers, (Obj*)StandardTokenizer_new());
-    VA_Push(analyzers, (Obj*)Normalizer_new(NULL, true, false));
-    VA_Push(analyzers, (Obj*)SnowStemmer_new(language));
-    PolyAnalyzer_init((PolyAnalyzer*)self, NULL, analyzers);
-    DECREF(analyzers);
+    Analyzer_init((Analyzer*)self);
+    self->language   = CB_Clone(language);
+    self->tokenizer  = StandardTokenizer_new();
+    self->normalizer = Normalizer_new(NULL, true, false);
+    self->stemmer    = SnowStemmer_new(language);
     return self;
+}
+
+void
+EasyAnalyzer_destroy(EasyAnalyzer *self) {
+    DECREF(self->language);
+    DECREF(self->tokenizer);
+    DECREF(self->normalizer);
+    DECREF(self->stemmer);
+    SUPER_DESTROY(self, EASYANALYZER);
+}
+
+Inversion*
+EasyAnalyzer_transform(EasyAnalyzer *self, Inversion *inversion) {
+    Inversion *inv1 = StandardTokenizer_Transform(self->tokenizer, inversion);
+    Inversion *inv2 = Normalizer_Transform(self->normalizer, inv1);
+    DECREF(inv1);
+    inv1 = SnowStemmer_Transform(self->stemmer, inv2);
+    DECREF(inv2);
+    return inv1;
+}
+
+Inversion*
+EasyAnalyzer_transform_text(EasyAnalyzer *self, CharBuf *text) {
+    Inversion *inv1 = StandardTokenizer_Transform_Text(self->tokenizer, text);
+    Inversion *inv2 = Normalizer_Transform(self->normalizer, inv1);
+    DECREF(inv1);
+    inv1 = SnowStemmer_Transform(self->stemmer, inv2);
+    DECREF(inv2);
+    return inv1;
+}
+
+Hash*
+EasyAnalyzer_dump(EasyAnalyzer *self) {
+    EasyAnalyzer_dump_t super_dump
+        = (EasyAnalyzer_dump_t)SUPER_METHOD(EASYANALYZER, EasyAnalyzer, Dump);
+    Hash *dump = super_dump(self);
+    Hash_Store_Str(dump, "language", 8, (Obj*)CB_Clone(self->language));
+    return dump;
+}
+
+EasyAnalyzer*
+EasyAnalyzer_load(EasyAnalyzer *self, Obj *dump) {
+    EasyAnalyzer_load_t super_load
+        = (EasyAnalyzer_load_t)SUPER_METHOD(EASYANALYZER, EasyAnalyzer, Load);
+    EasyAnalyzer *loaded = super_load(self, dump);
+    Hash    *source = (Hash*)CERTIFY(dump, HASH);
+    CharBuf *language
+        = (CharBuf*)CERTIFY(Hash_Fetch_Str(source, "language", 8), CHARBUF);
+    return EasyAnalyzer_init(loaded, language);
+}
+
+bool_t
+EasyAnalyzer_equals(EasyAnalyzer *self, Obj *other) {
+    EasyAnalyzer *const twin = (EasyAnalyzer*)other;
+    if (twin == self)                                     { return true; }
+    if (!Obj_Is_A(other, EASYANALYZER))                   { return false; }
+    if (!CB_Equals(twin->language, (Obj*)self->language)) { return false; }
+    return true;
 }
 
 
