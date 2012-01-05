8b38cf4babe90b6d485a05d00de2e884392ea529
Logan J. Bell <logie@apache.org>
2012-01-05 07:32:52 +0000

    First iteration of building binding CFC::Hierarchy to ruby. This work is associated with JIRA: 202.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1227510 13f79535-47bb-0310-9956-ffa450edef68

---
+++ clownfish/ruby/ext/Clownfish/CFC/extconf.rb
@@ -0,0 +1,9 @@
+require 'mkmf'
+CLOWNFISH_INCLUDE_DIR   = File.join('..','..','..','..','include')
+CLOWNFISH_SRC_DIR       = File.join('..','..','..','..','src')
+$CFLAGS = "-I#{CLOWNFISH_INCLUDE_DIR} -I#{CLOWNFISH_SRC_DIR}"
+$objs = ['Hierarchy.o']
+Dir.glob("#{CLOWNFISH_SRC_DIR}/*.o").each do|o|
+    $objs.push o
+end
+create_makefile 'Hierarchy'
