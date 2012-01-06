a97c8183656c3e59e79c981cb396e6de909e54d5
Logan J. Bell <logie@apache.org>
2012-01-06 22:52:51 +0000

    LUCY-202 1. Merged Clownfish/CFC/Hierarchy.c down to Clownfish/CFC.c 2. Namespaced correctly ie: Clownfish::CFC::Hierarchy.new 3. Corrected various variable names to match conventions from the perl side. 4. Updated extconf.rb to create Makefile.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1228482 13f79535-47bb-0310-9956-ffa450edef68

--- clownfish/ruby/ext/Clownfish/CFC.c
+++ clownfish/ruby/ext/Clownfish/CFC.c
@@ -17,3 +17,45 @@
 #include "ruby.h"
 #include "CFC.h"
 
+VALUE mClownfish;
+VALUE mCFC;
+VALUE cHierarchy;
+
+static VALUE cfc_hierarchy_alloc(VALUE klass) {
+    CFCHierarchy *self = CFCHierarchy_allocate();
+
+    VALUE self_rb = Data_Wrap_Struct(klass, 0, CFCHierarchy_destroy, self);
+
+    return self_rb;
+}
+
+static VALUE cfc_hierarchy_init(VALUE self_rb, VALUE source, VALUE dest) {
+
+    char *s = StringValuePtr(source);
+    char *d = StringValuePtr(dest);
+    CFCHierarchy *self;
+
+    Data_Get_Struct(self_rb, CFCHierarchy, self);
+    CFCHierarchy_init(self, s, d);
+
+    return self_rb;
+}
+
+static VALUE cfc_hierarchy_build(VALUE self_rb) {
+    CFCHierarchy *self;
+
+    Data_Get_Struct(self_rb, CFCHierarchy, self);
+    CFCHierarchy_build(self);
+}
+
+void Init_CFC() { 
+    mClownfish  = rb_define_module("Clownfish");
+    mCFC        = rb_define_module_under(mClownfish, "CFC");
+    cHierarchy  = rb_define_class_under(mCFC, "Hierarchy", rb_cObject);
+
+    rb_define_alloc_func(cHierarchy, cfc_hierarchy_alloc);
+
+    rb_define_method(cHierarchy, "initialize", cfc_hierarchy_init, 2);
+    rb_define_method(cHierarchy, "build", cfc_hierarchy_build, 0);
+}
+
