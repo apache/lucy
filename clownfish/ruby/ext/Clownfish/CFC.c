a57a5cb82a9a260354163b8f8440e55c5e0e2ca2
Logan J. Bell <logie@apache.org>
2012-01-07 23:09:16 +0000

    LUCY-202 1. Added Clownfish::CFC::Binding module. 2. Added Clownfish::CFC::Binding::Core class. 3. Created new and write_all_modified methods for the Core class.
    Ruby Implementation Examples:
    cf = Clownfish::CFC::Hierarchy.new("./cfsource","./cfdest")
    cf.build
    b = Clownfish::CFC::Binding::Core.new(cf,"./cfdest","","")
    b.write_all_modified
    b.write_all_modified(true)
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1228757 13f79535-47bb-0310-9956-ffa450edef68

--- clownfish/ruby/ext/Clownfish/CFC.c
+++ clownfish/ruby/ext/Clownfish/CFC.c
@@ -17,9 +17,42 @@
 #include "ruby.h"
 #include "CFC.h"
 
+static VALUE mBinding;
 static VALUE mClownfish;
 static VALUE mCFC;
+static VALUE cBindCore;
 static VALUE cHierarchy;
+
+static VALUE cfc_binding_core_alloc(VALUE klass) {
+    CFCBindCore *self = CFCBindCore_allocate();
+
+    VALUE self_rb = Data_Wrap_Struct(klass, 0, CFCBindCore_destroy, self);
+
+    return self_rb;
+}
+
+static VALUE cfc_binding_core_init(VALUE self_rb, VALUE hierarchy, VALUE dest, VALUE header, VALUE footer) {
+    CFCHierarchy* hierarchy_obj;
+    CFCBindCore *self;
+
+    Data_Get_Struct(hierarchy,CFCHierarchy,hierarchy_obj);
+    Data_Get_Struct(self_rb, CFCBindCore, self);
+
+    CFCBindCore_init(self, hierarchy_obj, StringValuePtr(dest), StringValuePtr(header), StringValuePtr(footer));
+
+    return self_rb;
+}
+
+static VALUE cfc_binding_core_write_all_modified(int argc, VALUE *argv, VALUE self_rb) {
+    CFCBindCore *self;
+
+    int modified = argc > 0 && RTEST(argv[0]) ? 1 : 0;
+
+    Data_Get_Struct(self_rb, CFCBindCore, self);
+    CFCBindCore_write_all_modified(self,modified);
+
+    return Qnil;
+}
 
 static VALUE cfc_hierarchy_alloc(VALUE klass) {
     CFCHierarchy *self = CFCHierarchy_allocate();
@@ -53,11 +86,16 @@ static VALUE cfc_hierarchy_build(VALUE self_rb) {
 void Init_CFC() { 
     mClownfish  = rb_define_module("Clownfish");
     mCFC        = rb_define_module_under(mClownfish, "CFC");
+    mBinding    = rb_define_module_under(mCFC, "Binding");
     cHierarchy  = rb_define_class_under(mCFC, "Hierarchy", rb_cObject);
+    cBindCore   = rb_define_class_under(mBinding, "Core", rb_cObject);
 
     rb_define_alloc_func(cHierarchy, cfc_hierarchy_alloc);
-
     rb_define_method(cHierarchy, "initialize", cfc_hierarchy_init, 2);
     rb_define_method(cHierarchy, "build", cfc_hierarchy_build, 0);
+
+    rb_define_alloc_func(cBindCore, cfc_binding_core_alloc);
+    rb_define_method(cBindCore, "initialize", cfc_binding_core_init, 4);
+    rb_define_method(cBindCore, "write_all_modified", cfc_binding_core_write_all_modified, -1);
 }
 
