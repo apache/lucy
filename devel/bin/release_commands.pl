7e581c363284d9f73075738681f146a543f32304
Peter Karman <karpet@apache.org>
2012-01-16 05:02:33 +0000

    fix release command script to use version variable
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/branches/0.3@1231838 13f79535-47bb-0310-9956-ffa450edef68

--- devel/bin/release_commands.pl
+++ devel/bin/release_commands.pl
@@ -80,12 +80,12 @@ say qq|perl -MDigest -e '\$d = Digest->new("MD5"); open \$fh, |
     . qq|"<apache-lucy-incubating-$x_y_z_version.tar.gz" or die; |
     . qq|\$d->addfile(\$fh); print \$d->hexdigest; |
     . qq|print "  apache-lucy-incubating-$x_y_z_version.tar.gz\\n"' > |
-    . qq| apache-lucy-incubating-0.2.2.tar.gz.md5|;
+    . qq| apache-lucy-incubating-$x_y_z_version.tar.gz.md5|;
 say qq|perl -MDigest -e '\$d = Digest->new("SHA-512"); open \$fh, |
     . qq|"<apache-lucy-incubating-$x_y_z_version.tar.gz" or die; |
     . qq|\$d->addfile(\$fh); print \$d->hexdigest; |
     . qq|print "  apache-lucy-incubating-$x_y_z_version.tar.gz\\n"' > |
-    . qq| apache-lucy-incubating-0.2.2.tar.gz.sha\n|;
+    . qq| apache-lucy-incubating-$x_y_z_version.tar.gz.sha\n|;
 
 say qq|# Sign the release.|;
 say qq|gpg --armor --output apache-lucy-incubating-$x_y_z_version.tar.gz.asc |
