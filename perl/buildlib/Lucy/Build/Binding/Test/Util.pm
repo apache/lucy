8deff27ab0080d44f372060bd38464c9c7da2f1e
Marvin Humphrey <marvin@apache.org>
2012-01-26 00:32:53 +0000

    Add strict and warnings.
    Add strict and warnings to Lucy::Build::Binding::*.pm.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1235996 13f79535-47bb-0310-9956-ffa450edef68

--- perl/buildlib/Lucy/Build/Binding/Test/Util.pm
+++ perl/buildlib/Lucy/Build/Binding/Test/Util.pm
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 package Lucy::Build::Binding::Test::Util;
+use strict;
+use warnings;
 
 sub bind_all {
     my $class = shift;
