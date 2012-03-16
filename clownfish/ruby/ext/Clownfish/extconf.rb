db090bd419be0a6c02d03ea000b47c5cea2d9def
Marvin Humphrey <marvin@apache.org>
2012-03-16 16:59:33 +0000

    Generalize filepath handling.
    Use filepath-handling code which is neutral with regards to both OS and
    compiler environment in CFC build.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucy/trunk@1301647 13f79535-47bb-0310-9956-ffa450edef68

--- clownfish/ruby/ext/Clownfish/extconf.rb
+++ clownfish/ruby/ext/Clownfish/extconf.rb
@@ -1,9 +1,13 @@
 require 'mkmf'
+require 'rbconfig'
+
 CLOWNFISH_INCLUDE_DIR   = File.join('..','..','..','include')
 CLOWNFISH_SRC_DIR       = File.join('..','..','..','src')
 $CFLAGS = "-I#{CLOWNFISH_INCLUDE_DIR} -I#{CLOWNFISH_SRC_DIR}"
-$objs = ['CFC.o']
-Dir.glob("#{CLOWNFISH_SRC_DIR}/*.o").each do|o|
+$objs = ['CFC.' + RbConfig::CONFIG['OBJEXT']]
+obj_glob = File.join(CLOWNFISH_SRC_DIR, '*.' + RbConfig::CONFIG['OBJEXT'])
+Dir.glob(obj_glob).each do|o|
     $objs.push o
 end
+
 create_makefile 'CFC'
