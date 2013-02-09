b676981f999d7f8fc973ee4a2b084549525ef6c8
Logan Bell <loganbell@gmail.com>
2013-02-09 15:29:02 -0800

    Modified includes to use ruby runtime path

--- clownfish/compiler/ruby/ext/Clownfish/extconf.rb
+++ clownfish/compiler/ruby/ext/Clownfish/extconf.rb
@@ -3,7 +3,8 @@ require 'rbconfig'
 
 CLOWNFISH_INCLUDE_DIR   = File.join('..','..','..','include')
 CLOWNFISH_SRC_DIR       = File.join('..','..','..','src')
-$CFLAGS = "-I#{CLOWNFISH_INCLUDE_DIR} -I#{CLOWNFISH_SRC_DIR}"
+CLOWNFISH_RUNTIME       = File.join('..','..','..','..','runtime','ruby')
+$CFLAGS = "-I#{CLOWNFISH_INCLUDE_DIR} -I#{CLOWNFISH_SRC_DIR} -I#{CLOWNFISH_RUNTIME}"
 $objs = ['CFC.' + RbConfig::CONFIG['OBJEXT']]
 obj_glob = File.join(CLOWNFISH_SRC_DIR, '*.' + RbConfig::CONFIG['OBJEXT'])
 Dir.glob(obj_glob).each do|o|
