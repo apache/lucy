985b469db8f055be898e37ef61d7ca64d024a91a
Nick Wellnhofer <wellnhofer@aevum.de>
2013-02-24 19:54:52 +0100

    Core tests for C bindings

---
+++ c/t/test_lucy.c
@@ -0,0 +1,32 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+
+#include "Clownfish/Test/TestFormatter.h"
+#include "Lucy/Test.h"
+
+int
+main() {
+    lucy_bootstrap_parcel();
+
+    lucy_TestFormatterCF *formatter = lucy_TestFormatterCF_new();
+    bool success = lucy_Test_run_all_batches((lucy_TestFormatter*)formatter);
+    CFISH_DECREF(formatter);
+
+    return success ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
