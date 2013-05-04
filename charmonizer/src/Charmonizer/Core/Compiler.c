c8ebd584a1e55dcd7526a7bf4452abaf55c5f97c
Nick Wellnhofer <wellnhofer@aevum.de>
2013-05-04 18:18:09 +0200

    Fix Cygwin detection

--- charmonizer/src/Charmonizer/Core/Compiler.c
+++ charmonizer/src/Charmonizer/Core/Compiler.c
@@ -364,7 +364,7 @@ chaz_CC_shared_lib_file(const char *name) {
     const char *prefix = "";
     const char *shlib_ext = chaz_OS_shared_lib_ext();
     if (!chaz_CC.intval__MSC_VER) {
-        if (strcmp(chaz_OS_name(), "cygwin") == 0) {
+        if (chaz_OS_is_cygwin()) {
             prefix = "cyg";
         }
         else {
