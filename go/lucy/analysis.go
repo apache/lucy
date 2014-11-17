1981c8214dbf224a6e0f5d9626481139c554d41e
Marvin Humphrey <marvin@rectangular.com>
2014-11-16 21:25:08 -0800

    Go stubs for Analyzer and EasyAnalyzer.

---
+++ go/lucy/analysis.go
@@ -0,0 +1,58 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lucy
+
+/*
+#include "Lucy/Analysis/Analyzer.h"
+#include "Lucy/Analysis/EasyAnalyzer.h"
+*/
+import "C"
+import "runtime"
+import "unsafe"
+
+import "git-wip-us.apache.org/repos/asf/lucy-clownfish.git/runtime/go/clownfish"
+
+type Analyzer interface {
+	clownfish.Obj
+	ToAnalyzerPtr() unsafe.Pointer
+}
+
+type EasyAnalyzer struct {
+	ref *C.lucy_EasyAnalyzer
+}
+
+func NewEasyAnalyzer(language string) *EasyAnalyzer {
+	lang := clownfish.NewString(language)
+	obj := &EasyAnalyzer{
+		C.lucy_EasyAnalyzer_new((*C.cfish_String)(lang.ToPtr())),
+	}
+	runtime.SetFinalizer(obj, (*EasyAnalyzer).finalize)
+	return obj
+}
+
+func (obj *EasyAnalyzer) finalize() {
+	C.cfish_dec_refcount(unsafe.Pointer(obj.ref))
+	obj.ref = nil
+}
+
+func (obj *EasyAnalyzer) ToPtr() unsafe.Pointer {
+	return unsafe.Pointer(obj.ref)
+}
+
+func (obj *EasyAnalyzer) ToAnalyzerPtr() unsafe.Pointer {
+	return obj.ToPtr()
+}
