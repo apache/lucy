0fdd4e39d57c1bc7a2005be8f345a67958f75d07
Nick Wellnhofer <wellnhofer@aevum.de>
2015-07-24 21:00:13 +0200

    Backport CI setup to 0.4 branch

---
+++ devel/bin/appveyor-build.bat
@@ -0,0 +1,41 @@
+@echo off
+
+if "%CLOWNFISH_HOST%" == "c" goto test_c
+if "%CLOWNFISH_HOST%" == "perl" goto test_perl
+
+echo unknown CLOWNFISH_HOST: %CLOWNFISH_HOST%
+exit /b 1
+
+:test_c
+
+if "%MSVC_VERSION%" == "10" goto msvc_10
+
+call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" amd64
+goto msvc_build
+
+:msvc_10
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+
+:msvc_build
+
+rem Install Clownfish.
+cd \projects
+git clone -q -b 0.4 --depth 1 https://git-wip-us.apache.org/repos/asf/lucy-clownfish.git
+cd lucy-clownfish\runtime\c
+call configure && nmake || exit /b
+call install --prefix C:\install
+
+cd \projects\lucy\c
+call configure --clownfish-prefix=C:\install && nmake && nmake test
+
+exit /b
+
+:test_perl
+
+perl -V
+
+cd perl
+perl Build.PL && call Build && call Build test
+
+exit /b
+
---
+++ devel/bin/travis-test.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Exit if any command returns non-zero.
+set -e
+
+# Print all commands before executing.
+set -x
+
+test_c() {
+    # Install Clownfish.
+    git clone -q -b 0.4 --depth 1 https://git-wip-us.apache.org/repos/asf/lucy-clownfish.git
+    cd lucy-clownfish/runtime/c
+    ./configure
+    make -j
+    sudo ./install.sh --prefix /usr/local
+    sudo ldconfig
+
+    cd ../../../c
+    ./configure
+    make -j test
+}
+
+test_perl() {
+    perlbrew switch $PERL_VERSION
+
+    # Install Clownfish.
+    git clone -q -b 0.4 --depth 1 https://git-wip-us.apache.org/repos/asf/lucy-clownfish.git
+    cd lucy-clownfish/runtime/perl
+    perl Build.PL
+    ./Build
+    sudo ./Build install
+    cd ../../compiler/perl
+    sudo ./Build install
+
+    cd ../../../perl
+    perl Build.PL
+    ./Build test
+}
+
+case $CLOWNFISH_HOST in
+    perl)
+        test_perl
+        ;;
+    c)
+        test_c
+        ;;
+    *)
+        echo "unknown CLOWNFISH_HOST: $CLOWNFISH_HOST"
+        exit 1
+esac
+
