apache/lucy

commit 633c4d30b74dbd69dbfc2c70c01c7638317c2d6a

Remove RegexTokenizer_Set_Token_RE.
It was used internally a long time ago and is now obsolete.

perl/xs/Lucy/Analysis/RegexTokenizer.c
@@ -27,13 +27,6 @@
 static SV*
 S_compile_token_re(pTHX_ cfish_String *pattern);
 
-static void
-S_set_token_re_but_not_pattern(pTHX_ lucy_RegexTokenizer *self,
-                               void *token_re);
-
-static void
-S_set_pattern_from_token_re(pTHX_ lucy_RegexTokenizer *self, void *token_re);
-
 bool
 lucy_RegexTokenizer_is_available(void) {
     return true;
@@ -61,9 +54,26 @@ lucy_RegexTokenizer_init(lucy_RegexTokenizer *self,
 
     // Acquire a compiled regex engine for matching one token.
     dTHX;
-    SV *token_re_sv = S_compile_token_re(aTHX_ ivars->pattern);
-    S_set_token_re_but_not_pattern(aTHX_ self, SvRV(token_re_sv));
-    SvREFCNT_dec(token_re_sv);
+    SV *token_re = S_compile_token_re(aTHX_ ivars->pattern);
+#if (PERL_VERSION > 10)
+    REGEXP *rx = SvRX((SV*)token_re);
+#else
+    MAGIC *magic = NULL;
+    if (SvMAGICAL((SV*)token_re)) {
+        magic = mg_find((SV*)token_re, PERL_MAGIC_qr);
+    }
+    if (!magic) {
+        THROW(CFISH_ERR, "token_re is not a qr// entity");
+    }
+    REGEXP *rx = (REGEXP*)magic->mg_obj;
+#endif
+    if (rx == NULL) {
+        THROW(CFISH_ERR, "Failed to extract REGEXP from token_re '%s'",
+              SvPV_nolen((SV*)token_re));
+    }
+    ivars->token_re = rx;
+    (void)ReREFCNT_inc(((REGEXP*)ivars->token_re));
+    SvREFCNT_dec(token_re);
 
     return self;
 }
@@ -85,51 +95,6 @@ S_compile_token_re(pTHX_ cfish_String *pattern) {
     FREETMPS;
     LEAVE;
     return token_re_sv;
-}
-
-static void
-S_set_token_re_but_not_pattern(pTHX_ lucy_RegexTokenizer *self,
-                               void *token_re) {
-    lucy_RegexTokenizerIVARS *const ivars = lucy_RegexTokenizer_IVARS(self);
-#if (PERL_VERSION > 10)
-    REGEXP *rx = SvRX((SV*)token_re);
-#else
-    MAGIC *magic = NULL;
-    if (SvMAGICAL((SV*)token_re)) {
-        magic = mg_find((SV*)token_re, PERL_MAGIC_qr);
-    }
-    if (!magic) {
-        THROW(CFISH_ERR, "token_re is not a qr// entity");
-    }
-    REGEXP *rx = (REGEXP*)magic->mg_obj;
-#endif
-    if (rx == NULL) {
-        THROW(CFISH_ERR, "Failed to extract REGEXP from token_re '%s'",
-              SvPV_nolen((SV*)token_re));
-    }
-    if (ivars->token_re) { ReREFCNT_dec(((REGEXP*)ivars->token_re)); }
-    ivars->token_re = rx;
-    (void)ReREFCNT_inc(((REGEXP*)ivars->token_re));
-}
-
-static void
-S_set_pattern_from_token_re(pTHX_ lucy_RegexTokenizer *self, void *token_re) {
-    lucy_RegexTokenizerIVARS *const ivars = lucy_RegexTokenizer_IVARS(self);
-    SV *rv = newRV((SV*)token_re);
-    STRLEN len = 0;
-    char *ptr = SvPVutf8((SV*)rv, len);
-    CFISH_DECREF(ivars->pattern);
-    ivars->pattern = cfish_Str_new_from_trusted_utf8(ptr, len);
-    SvREFCNT_dec(rv);
-}
-
-void
-LUCY_RegexTokenizer_Set_Token_RE_IMP(lucy_RegexTokenizer *self,
-                                     void *token_re) {
-    dTHX;
-    S_set_token_re_but_not_pattern(aTHX_ self, token_re);
-    // Set pattern as a side effect.
-    S_set_pattern_from_token_re(aTHX_ self, token_re);
 }
 
 void

c/src/Lucy/Analysis/RegexTokenizer.c
@@ -87,13 +87,6 @@ RegexTokenizer_init(RegexTokenizer *self, String *pattern) {
 }
 
 void
-RegexTokenizer_Set_Token_RE_IMP(RegexTokenizer *self, void *token_re) {
-    UNUSED_VAR(self);
-    UNUSED_VAR(token_re);
-    THROW(ERR, "TODO");
-}
-
-void
 RegexTokenizer_Destroy_IMP(RegexTokenizer *self) {
     RegexTokenizerIVARS *const ivars = RegexTokenizer_IVARS(self);
     DECREF(ivars->pattern);

example-lang/src/Lucy/Analysis/RegexTokenizer.c
@@ -29,11 +29,6 @@ lucy_RegexTokenizer_init(lucy_RegexTokenizer *self,
 }
 
 void
-lucy_RegexTokenizer_set_token_re(lucy_RegexTokenizer *self, void *token_re) {
-    THROW(LUCY_ERR, "TODO");
-}
-
-void
 lucy_RegexTokenizer_destroy(lucy_RegexTokenizer *self) {
     THROW(LUCY_ERR, "TODO");
 }

core/Lucy/Analysis/RegexTokenizer.cfh
@@ -84,12 +84,6 @@ public class Lucy::Analysis::RegexTokenizer
     Tokenize_Utf8(RegexTokenizer *self, const char *text, size_t len,
                   Inversion *inversion);
 
-    /** Set the compiled regular expression for matching a token.  Also sets
-     * `pattern` as a side effect.
-     */
-    void
-    Set_Token_RE(RegexTokenizer *self, void *token_re);
-
     public incremented Obj*
     Dump(RegexTokenizer *self);
 

commit 632a46b8b8df694d320cda2bcec72d5248e9a49f

Dereference token_re on older Perls
Broken in commit 633c4d3.

Fixes LUCY-297.

perl/xs/Lucy/Analysis/RegexTokenizer.c
@@ -58,9 +58,13 @@ lucy_RegexTokenizer_init(lucy_RegexTokenizer *self,
 #if (PERL_VERSION > 10)
     REGEXP *rx = SvRX((SV*)token_re);
 #else
+    if (!SvROK(token_re)) {
+        THROW(CFISH_ERR, "token_re is not a qr// entity");
+    }
+    SV *inner = SvRV(token_re);
     MAGIC *magic = NULL;
-    if (SvMAGICAL((SV*)token_re)) {
-        magic = mg_find((SV*)token_re, PERL_MAGIC_qr);
+    if (SvMAGICAL((SV*)inner)) {
+        magic = mg_find((SV*)inner, PERL_MAGIC_qr);
     }
     if (!magic) {
         THROW(CFISH_ERR, "token_re is not a qr// entity");